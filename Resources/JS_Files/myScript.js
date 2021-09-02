ca7685c99fb21a8b8ee96adf1042bd4cee92124e
SallamRady <53578065+SallamRady@users.noreply.github.com>
2021-09-02 22:36:24 +0200

    upload Design Template
    upload Design Template Files

---
+++ Resources/JS_Files/myScript.js
@@ -0,0 +1,17 @@
+/* Go to Top of Page */
+window.onscroll = () => {
+    let topPage = document.getElementById('top-page-arrow');
+    window.scrollY > 500 ? (topPage.style.display = 'block')
+        : (topPage.style.display = 'none');
+
+    let ourNavbar = document.getElementById('ourNavbar');
+    window.scrollY > 320 ? (ourNavbar.classList.add('fixed-top'))
+    : (ourNavbar.classList.remove('fixed-top'));
+}
+function goToTop(){
+   window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth' 
+   });
+}
